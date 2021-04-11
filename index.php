32b2b26b0f71b77c24b14ad1a137c43888647cfa
gillyhuga <gilly.huga@yahoo.co.id>
2021-04-11 16:06:04 +0700

    initial  commit

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Giltech</title>
+</head>
+
+<body>
+    <header>
+        <nav class="nav container">
+            <div>
+                <a href="/" class="nav_logo ">Giltech.</a>
+            </div>
+            <div>
+                <ul class="nav_list">
+                    <li class="nav_item"><a href="/" class="nav_link">Home</a></li>
+                    <li class="nav_item"><a href="#wrapper" class="nav_link">Blog</a></li>
+                    <li class="nav_item"><a href="profile.php" class="nav_link">Profile</a></li>
+                </ul>
+            </div>
+        </nav>
+
+    </header>
+    <div class="hero-image">
+        <div class="hero-text container">
+            <h1>Temukan Berita Terkini Tentang Teknologi</h1>
+            <p>Situs berita yang mengulas serba-serbi teknologi</p>
+        </div>
+    </div>
+    <div class="header-bg">&nbsp;</div>
+    <section id="wrapper">
+        <aside id="left" class="container">
+            <article class="all-content">
+                <h1>Kategori</h1>
+                <article class="content  category-sidebar">
+                    <li><a class="category-sidebar" href="#computer-content">Computer</a></li>
+                </article>
+                <article class="content  category-sidebar">
+                    <li><a class="category-sidebar" href="#gadget-content">Gadget</a></li>
+                </article>
+                <article class="content  category-sidebar">
+                    <li><a class="category-sidebar" href="#games-content">Games</a></li>
+                </article>
+                <article class="content  category-sidebar">
+                    <li><a class="category-sidebar" href="#internet-content">Internet</a></li>
+                </article>
+            </article>
+            </article>
+        </aside>
+        <div id="mid">
+            <article id="computer-content" class="all-content">
+                <h1>Computer</h1>
+                <article class="content">
+                    <h2>Update Windows 10 di April 2018; Apa Saja yang Baru?</h2>
+                    <p class="author">By Jimmy Ahyari / June 16, 2020</p>
+                    <img class="image-content" src="assets/images/computer-1.jpg">
+                    <p>Dengan Windows 10, Microsoft telah memfokuskan diri ­untuk menghadirkan sistem operasi yang
+                        paling modern dan paling aman. Sejak peluncuran awal, Microsoft telah menghadirkan dua
+                        pembaharuan (updates) dengan fitur-fitur yang dirancang untuk menghadirkan kemampuan
+                        kreativitas di dalam diri kita masing-masing.</p>
+                    <a href="https://teknologi.info/304106/update-windows-10-april-2018/"
+                        class="button">Selengkapnya</a>
+                </article>
+                <article class="content">
+                    <h2>Wow! Seagate Tawarkan Hard Disk Drive Berkapasitas 10 TB (Terabyte)!</h2>
+                    <p class="author">By Jimmy Ahyari / June 16, 2020</p>
+                    <img class="image-content" src="assets/images/computer-2.jpg">
+                    <p>Seagate merilis HDD (Hard Disk Drive) dengan kemampuan penyimpanan 10 TeraByte (TB) berhelium
+                        untuk segmentasi enterprise. Harddisk 3,5″ bergaransi 5 tahun tersebut memiliki 2 versi, yaitu
+                        SATA (6 GB/detik) dan SAS (12 GB/detik).</p>
+                    <a href="https://teknologi.info/212530/seagate-hard-disk-drive-10-tb-terabyte/"
+                        class="button">Selengkapnya</a>
+                </article>
+            </article>
+
+            <article id="gadget-content" class="all-content">
+                <h1>Gadget</h1>
+                <article class="content">
+                    <h2>Apple Watch Sekarang dapat Memutar Spotify Tanpa iPhone</h2>
+                    <p class="author">By Jimmy Ahyari / June 16, 2020</p>
+                    <img class="image-content" src="assets/images/gadget-1.jpg">
+                    <p>Pengguna Apple Watch mungkin akan bersorak setelah membaca berita ini. Benar! Sekarang Anda bisa
+                        langsung menemukan dan memainkan lagu favorit dari jam tangan pintar Anda, meskipun tanpa adanya
+                        kehadiran iPhone.</p>
+                    <a href="https://teknologi.info/257394/apple-watch-dapat-memutar-spotify-tanpa-iphone/"
+                        class="button">Selengkapnya</a>
+                </article>
+            </article>
+            <article id="games-content" class="all-content">
+                <h1>Games</h1>
+                <article class="content">
+                    <h2>Microsoft: Performa Xbox Series X Tinggal Persoalan Optimasi Saja</h2>
+                    <p class="author">By Jimmy Ahyari / June 16, 2020</p>
+                    <img class="image-content" src="assets/images/games-1.jpg">
+                    <p>Pertempuran Microsoft dan Sony sudah dari dulu; jauh sebelum konsol terbaru mereka dirilis.
+                        Sekarang, setelah Xbox Series X dan PlayStation 5 keluar, persaingan menjadi semakin panas. Kali
+                        ini, bagaimanapun, yang akan menjadi juri adalah para gamer dan para kritikus, dan beberapa di
+                        antaranya sudah menyampaikan bahwa Microsoft gagal memenuhi kebanggaannya sebagai “konsol paling
+                        kuat di dunia”.</p>
+                    <a href="https://teknologi.info/277421/microsoft-performa-xbox-series-x-tinggal-persoalan-optimasi-saja/"
+                        class="button">Selengkapnya</a>
+                </article>
+                <article class="content">
+                    <h2>Pertama di Asia Tenggara, PlayStation Experience Siap Digelar</h2>
+                    <p class="author">By Jimmy Ahyari / June 16, 2020</p>
+                    <img class="image-content" src="assets/images/games-2.jpg">
+                    <p>Sony Interactive Entertainment Hong Kong mengumumkan PlayStation Experience 2017 siap
+                        diselenggarakan di Kuala Lumpur, Malaysia, pada 5 Agustus mendatang. Di Amerika Serikat sendiri
+                        ajang ini akan tetap digelar seperti biasa.</p>
+                    <a href="https://teknologi.info/123786/playstation-experience-asia-tenggara-siap-digelar/"
+                        class="button">Selengkapnya</a>
+                </article>
+            </article>
+            <article id="internet-content" class="all-content">
+                <h1>Internet</h1>
+                <article class="content">
+                    <h2>Google Rilis Data Penelusuran Tertinggi tahun 2020</h2>
+                    <p class="author">By Jimmy Ahyari / June 16, 2020</p>
+                    <img class="image-content" src="assets/images/internet-1.jpg">
+                    <p>Inilah saatnya ketika Google mengungkapkan penelusuran teratasnya selama 12 bulan terakhir dan
+                        memberikan gambaran kepada publik tentang apa yang paling diminati oleh banyak orang di dunia
+                        (termasuk Indonesia). Hasil tahun ini sama sekali tidak mengejutkan secara global. Mungkin Anda
+                        sudah bisa menebak tentang penelusuran teratas karena bisa jadi Anda juga sering mencarinya.</p>
+                    <a href="https://teknologi.info/147598/google-rilis-data-penelusuran-tertinggi-2020/"
+                        class="button">Selengkapnya</a>
+                </article>
+            </article>
+        </div>
+        <aside id="right" class="container">
+            <div class="search">
+                <input type="text" class="searchForm" placeholder="Cari berita apa?">
+                <button type="submit" class="searchButton">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+            <article class="all-content">
+                <h1>Sedang Populer</h1>
+                <article class="content">
+                    <h2>Microsoft: Performa Xbox Series X Tinggal Persoalan Optimasi Saja</h2>
+                    <p>Pertempuran Microsoft dan Sony sudah dari dulu; jauh sebelum konsol terbaru mereka dirilis.
+                        Sekarang, setelah Xbox Series X dan PlayStation 5 keluar...</p>
+                    <a href="https://teknologi.info/277421/microsoft-performa-xbox-series-x-tinggal-persoalan-optimasi-saja/"
+                        class="button">Selengkapnya</a>
+                </article>
+                <article class="content">
+                    <h2>Apple Watch Sekarang dapat Memutar Spotify Tanpa iPhone</h2>
+                    <p>Pengguna Apple Watch mungkin akan bersorak setelah membaca berita ini. Benar! Sekarang Anda bisa
+                        langsung...</p>
+                    <a href="https://teknologi.info/257394/apple-watch-dapat-memutar-spotify-tanpa-iphone/"
+                        class="button">Selengkapnya</a>
+                </article>
+                <article class="content">
+                    <h2>Update Windows 10 di April 2018; Apa Saja yang Baru?</h2>
+                    <p>Dengan Windows 10, Microsoft telah memfokuskan diri ­untuk menghadirkan sistem operasi yang
+                        paling modern dan paling...</p>
+                    <a href="https://teknologi.info/304106/update-windows-10-april-2018/"
+                        class="button">Selengkapnya</a>
+                </article>
+            </article>
+            </article>
+        </aside>
+    </section>
+
+    <footer class="footer">
+        <div>
+            <p> &copy; Gilly Huga Anargya 2021</p>
+        </div>
+    </footer>
+
+</body>
+
+<script src="assets/js/main.js"></script>
+
+</html>
