263bbb87ae36224957d174cca80acce00f8282e5
Gilly <gilly.huga@yahoo.co.id>
2021-09-12 19:43:17 +0700

    change path

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -80,6 +80,6 @@
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
 
-        <link rel="stylesheet" href="{{asset('/css/admin.css')}}">
+        <link rel="stylesheet" href="/css/admin.css">
     </body>
 </html>
